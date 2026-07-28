af72bcdbd806c569c6ecee31222091f24df6d404
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-28 23:39:54 +0330

    Add English translations for backend modules

---
+++ contao/languages/en/modules.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This file is part of Contao Clients.
+ *
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+$GLOBALS['TL_LANG']['MOD']['company'] = ['Company', 'Manage company-related data'];
+$GLOBALS['TL_LANG']['MOD']['company_clients'] = ['Clients', 'Manage company clients'];
