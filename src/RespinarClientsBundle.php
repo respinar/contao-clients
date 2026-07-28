11c9329ba227aee5ddbe83c37785c9346f021fb8
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-28 21:37:31 +0330

    Add RespinarClientsBundle with getPath() for Contao resource discovery

---
+++ src/RespinarClientsBundle.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Clients.
+ *
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+namespace Respinar\ClientsBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class RespinarClientsBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    {
+        $containerConfigurator->import('../config/services.yaml');
+    }
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+}
