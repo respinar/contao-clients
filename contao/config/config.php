42d596cd1d80a954ca36e48e3e6d6c8059b02fda
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-28 23:39:35 +0330

    Add backend module registration and model mapping

---
+++ contao/config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Contao Clients.
+ *
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+use Respinar\ClientsBundle\Model\CompanyClientModel;
+
+/*
+ * Backend modules
+ */
+$GLOBALS['BE_MOD']['company']['company_clients'] = [
+    'tables' => ['tl_company_client']
+];
+
+/*
+ * Models
+ */
+$GLOBALS['TL_MODELS']['tl_company_client'] = CompanyClientModel::class;
