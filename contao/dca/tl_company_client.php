44839f9ab3b1f308c4bafcadd02597eb6573bb10
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-28 23:39:43 +0330

    Add DCA for tl_company_client with logo, description, and publish fields

---
+++ contao/dca/tl_company_client.php
@@ -0,0 +1,149 @@
+<?php
+
+/*
+ * This file is part of Contao Clients.
+ *
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+use Contao\Backend;
+use Contao\DC_Table;
+
+$GLOBALS['TL_DCA']['tl_company_client'] = [
+    'config' => [
+        'dataContainer'    => DC_Table::class,
+        'enableVersioning' => true,
+        'switchToEdit'     => true,
+        'sql'              => [
+            'keys' => [
+                'id'        => 'primary',
+                'alias'     => 'index',
+                'published' => 'index',
+            ],
+        ],
+    ],
+
+    'list' => [
+        'sorting' => [
+            'mode'        => 1,
+            'fields'      => ['name'],
+            'flag'        => 1,
+            'panelLayout' => 'filter;sort,search,limit',
+        ],
+        'label' => [
+            'fields' => ['name', 'industry'],
+            'format' => '%s <span style="color:#999;padding-left:3px">[%s]</span>',
+        ],
+        'global_operations' => [
+            'all' => [
+                'href'       => 'act=select',
+                'class'      => 'header_edit_all',
+                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
+            ],
+        ],
+        'operations' => [
+            'edit'   => [
+                'href'  => 'act=edit',
+                'icon'  => 'edit.svg',
+            ],
+            'copy'   => [
+                'href'  => 'act=copy',
+                'icon'  => 'copy.svg',
+            ],
+            'delete' => [
+                'href'       => 'act=delete',
+                'icon'       => 'delete.svg',
+                'attributes' => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '') . '\'))return false;Backend.getScrollOffset()"',
+            ],
+            'show'   => [
+                'href'  => 'act=show',
+                'icon'  => 'show.svg',
+            ],
+            'toggle' => [
+                'haste_ajax_operation' => [
+                    'field'            => 'published',
+                    'options'          => [
+                        ['value' => '', 'icon' => 'invisible.svg'],
+                        ['value' => '1', 'icon' => 'visible.svg'],
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'palettes' => [
+        'default' => '{title_legend},name,alias;{logo_legend},logo;{details_legend},website,description,industry,location;{publish_legend},published,start,stop',
+    ],
+
+    'fields' => [
+        'id' => [
+            'sql' => 'int(10) unsigned NOT NULL auto_increment',
+        ],
+        'tstamp' => [
+            'sql' => 'int(10) unsigned NOT NULL default 0',
+        ],
+        'name' => [
+            'search'    => true,
+            'sorting'   => true,
+            'flag'      => 1,
+            'inputType' => 'text',
+            'eval'      => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+            'sql'       => "varchar(255) NOT NULL default ''",
+        ],
+        'alias' => [
+            'search'    => true,
+            'inputType' => 'text',
+            'eval'      => ['rgxp' => 'alias', 'doNotCopy' => true, 'maxlength' => 128, 'tl_class' => 'w50'],
+            'sql'       => "varchar(128) COLLATE utf8mb4_bin NOT NULL default ''",
+        ],
+        'logo' => [
+            'inputType' => 'fileTree',
+            'eval'      => ['filesOnly' => true, 'fieldType' => 'radio', 'extensions' => 'jpg,jpeg,png,gif,svg,webp', 'tl_class' => 'clr'],
+            'sql'       => 'binary(16) NULL',
+        ],
+        'website' => [
+            'search'    => true,
+            'inputType' => 'text',
+            'eval'      => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
+            'sql'       => "varchar(255) NOT NULL default ''",
+        ],
+        'description' => [
+            'search'    => true,
+            'inputType' => 'textarea',
+            'eval'      => ['rte' => 'tinyMCE', 'tl_class' => 'clr'],
+            'sql'       => 'mediumtext NULL',
+        ],
+        'industry' => [
+            'filter'    => true,
+            'search'    => true,
+            'inputType' => 'text',
+            'eval'      => ['maxlength' => 255, 'tl_class' => 'w50'],
+            'sql'       => "varchar(255) NOT NULL default ''",
+        ],
+        'location' => [
+            'search'    => true,
+            'inputType' => 'text',
+            'eval'      => ['maxlength' => 255, 'tl_class' => 'w50'],
+            'sql'       => "varchar(255) NOT NULL default ''",
+        ],
+        'published' => [
+            'toggle'    => true,
+            'filter'    => true,
+            'inputType' => 'checkbox',
+            'eval'      => ['doNotCopy' => true],
+            'sql'       => "char(1) NOT NULL default ''",
+        ],
+        'start' => [
+            'inputType' => 'text',
+            'eval'      => ['rgxp' => 'datim', 'datepicker' => true, 'tl_class' => 'w50 wizard'],
+            'sql'       => "varchar(10) NOT NULL default ''",
+        ],
+        'stop' => [
+            'inputType' => 'text',
+            'eval'      => ['rgxp' => 'datim', 'datepicker' => true, 'tl_class' => 'w50 wizard'],
+            'sql'       => "varchar(10) NOT NULL default ''",
+        ],
+    ],
+];
