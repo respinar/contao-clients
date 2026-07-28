c794b4f0d5750068f6784f8ced7573f12b8d7678
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-28 23:41:05 +0330

    Add CompanyClientModel for reusable reference in other bundles

---
+++ src/Model/CompanyClientModel.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Clients.
+ *
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+namespace Respinar\ClientsBundle\Model;
+
+use Contao\Model;
+
+class CompanyClientModel extends Model
+{
+    protected static $strTable = 'tl_company_client';
+}
