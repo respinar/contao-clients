49e13fe6b03398c321ae915bcee9879ffcd6a372
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-30 09:10:42 +0330

    Clean up client DCA: remove unused import, switchToEdit, and default operations

--- contao/dca/tl_company_client.php
+++ contao/dca/tl_company_client.php
@@ -8,14 +8,12 @@
  * @license MIT
  */
 
-use Contao\Backend;
 use Contao\DC_Table;
 
 $GLOBALS['TL_DCA']['tl_company_client'] = [
     'config' => [
         'dataContainer'    => DC_Table::class,
         'enableVersioning' => true,
-        'switchToEdit'     => true,
         'ptable'           => 'tl_company_client_group',
         'sql'              => [
             'keys' => [
@@ -33,41 +31,6 @@ $GLOBALS['TL_DCA']['tl_company_client'] = [
             'flag'         => 1,
             'headerFields' => ['title', 'alias'],
             'panelLayout'  => 'filter;sort,search,limit',
-        ],
-        'global_operations' => [
-            'all' => [
-                'href'       => 'act=select',
-                'class'      => 'header_edit_all',
-                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
-            ],
-        ],
-        'operations' => [
-            'edit'   => [
-                'href'  => 'act=edit',
-                'icon'  => 'edit.svg',
-            ],
-            'copy'   => [
-                'href'  => 'act=copy',
-                'icon'  => 'copy.svg',
-            ],
-            'delete' => [
-                'href'       => 'act=delete',
-                'icon'       => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '') . '\'))return false;Backend.getScrollOffset()"',
-            ],
-            'show'   => [
-                'href'  => 'act=show',
-                'icon'  => 'show.svg',
-            ],
-            'toggle' => [
-                'haste_ajax_operation' => [
-                    'field'            => 'published',
-                    'options'          => [
-                        ['value' => '', 'icon' => 'invisible.svg'],
-                        ['value' => '1', 'icon' => 'visible.svg'],
-                    ],
-                ],
-            ],
         ],
     ],
 
