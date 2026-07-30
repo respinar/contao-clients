c9dd4f6962f1dd9427d696b8f2939c4a7105be92
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-30 07:55:09 +0330

    Add English translations for client group DCA

---
+++ contao/languages/en/tl_company_client_group.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Contao Clients.
+ *
+ * (c) Hamid Peywasti
+ *
+ * @license MIT
+ */
+
+$GLOBALS['TL_LANG']['tl_company_client_group'] = [
+    // Legends
+    'title_legend' => 'Title and alias',
+
+    // Fields
+    'title' => ['Title', 'Please enter the group title.'],
+    'alias' => ['Alias', 'The alias is a unique reference that can be used instead of the numeric ID.'],
+
+    // Operations
+    'new'    => ['New group', 'Create a new client group'],
+    'edit'   => ['Manage clients', 'Manage clients in group ID %s'],
+    'editheader' => ['Edit group', 'Edit group ID %s'],
+    'copy'   => ['Duplicate group', 'Duplicate group ID %s'],
+    'delete' => ['Delete group', 'Delete group ID %s'],
+    'show'   => ['Group details', 'Show details of group ID %s'],
+
+    // Global operations
+    'all' => ['Edit multiple', 'Edit multiple groups at once'],
+];
