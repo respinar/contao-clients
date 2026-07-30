e51378027ffe695d141279cbc55dab9244862e7b
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-30 17:49:25 +0330

    Add clients mount field to tl_user_group for group-level access control

---
+++ contao/dca/tl_user_group.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Clients Bundle.
+ *
+ * (c) Hamid Peywasti
+ *
+ * @license MIT
+ */
+
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+use Doctrine\DBAL\Platforms\AbstractMySQLPlatform;
+
+// Extend the default palette
+PaletteManipulator::create()
+	->addLegend('clients_legend', 'amg_legend', PaletteManipulator::POSITION_BEFORE)
+	->addField('clients', 'clients_legend', PaletteManipulator::POSITION_APPEND)
+	->applyToPalette('default', 'tl_user_group')
+;
+
+// Add fields to tl_user_group
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['clients'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_user']['clients'],
+	'inputType'               => 'checkbox',
+	'foreignKey'              => 'tl_company_client_group.title',
+	'eval'                    => array('multiple'=>true),
+	'sql'                     => array('type'=>'blob', 'length'=>AbstractMySQLPlatform::LENGTH_LIMIT_BLOB, 'notnull'=>false),
+	'relation'                => array('type'=>'hasMany', 'load'=>'lazy')
+);
