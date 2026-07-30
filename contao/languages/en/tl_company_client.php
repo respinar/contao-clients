7c8f64ec376143d7d23630b2abf77ba4a583c35d
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-30 20:47:50 +0330

    ECS: Add strict_types declaration and fix alignment in client translation

--- contao/languages/en/tl_company_client.php
+++ contao/languages/en/tl_company_client.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Clients.
  *
@@ -10,34 +12,34 @@
 
 $GLOBALS['TL_LANG']['tl_company_client'] = [
     // Legends
-    'title_legend'       => 'Name and alias',
-    'logo_legend'        => 'Logo',
-    'details_legend'     => 'Details',
-    'category_legend'    => 'Categories',
-    'publish_legend'     => 'Publish settings',
+    'title_legend' => 'Name and alias',
+    'logo_legend' => 'Logo',
+    'details_legend' => 'Details',
+    'category_legend' => 'Categories',
+    'publish_legend' => 'Publish settings',
 
     // Fields
-    'pid'                => ['Group', 'The client group this client belongs to.'],
-    'name'               => ['Name', 'Please enter the client/company name.'],
-    'alias'              => ['Alias', 'The alias is a unique reference that can be used instead of the numeric ID.'],
-    'logo'               => ['Logo', 'Please select a client logo.'],
-    'website'            => ['Website', 'Please enter an external website URL.'],
-    'description'        => ['Description', 'Please enter a description for this client.'],
-    'industry'           => ['Industry', 'Please enter the industry this client operates in.'],
-    'location'           => ['Location', 'Please enter the client location.'],
-    'categories'         => ['Categories', 'Please select one or more categories.'],
-    'published'          => ['Publish client', 'Make the client visible on the website.'],
-    'start'              => ['Show from', 'Do not show the client on the website before this date.'],
-    'stop'               => ['Show until', 'Do not show the client on the website after this date.'],
+    'pid' => ['Group', 'The client group this client belongs to.'],
+    'name' => ['Name', 'Please enter the client/company name.'],
+    'alias' => ['Alias', 'The alias is a unique reference that can be used instead of the numeric ID.'],
+    'logo' => ['Logo', 'Please select a client logo.'],
+    'website' => ['Website', 'Please enter an external website URL.'],
+    'description' => ['Description', 'Please enter a description for this client.'],
+    'industry' => ['Industry', 'Please enter the industry this client operates in.'],
+    'location' => ['Location', 'Please enter the client location.'],
+    'categories' => ['Categories', 'Please select one or more categories.'],
+    'published' => ['Publish client', 'Make the client visible on the website.'],
+    'start' => ['Show from', 'Do not show the client on the website before this date.'],
+    'stop' => ['Show until', 'Do not show the client on the website after this date.'],
 
     // Operations
-    'new'                => ['New client', 'Create a new client'],
-    'edit'               => ['Edit client', 'Edit client ID %s'],
-    'copy'               => ['Duplicate client', 'Duplicate client ID %s'],
-    'delete'             => ['Delete client', 'Delete client ID %s'],
-    'show'               => ['Client details', 'Show details of client ID %s'],
-    'toggle'             => ['Publish/unpublish client', 'Publish/unpublish client ID %s'],
+    'new' => ['New client', 'Create a new client'],
+    'edit' => ['Edit client', 'Edit client ID %s'],
+    'copy' => ['Duplicate client', 'Duplicate client ID %s'],
+    'delete' => ['Delete client', 'Delete client ID %s'],
+    'show' => ['Client details', 'Show details of client ID %s'],
+    'toggle' => ['Publish/unpublish client', 'Publish/unpublish client ID %s'],
 
     // Global operations
-    'all'                => ['Edit multiple', 'Edit multiple clients at once'],
+    'all' => ['Edit multiple', 'Edit multiple clients at once'],
 ];
