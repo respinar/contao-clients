d04a9653bdb2ea2b6295ca56d5db951b005dfa83
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-30 20:46:20 +0330

    ECS: Convert tabs to spaces and array syntax in tl_user DCA

--- contao/dca/tl_user.php
+++ contao/dca/tl_user.php
@@ -15,18 +15,18 @@ use Doctrine\DBAL\Platforms\AbstractMySQLPlatform;
 
 // Extend the default palettes
 PaletteManipulator::create()
-	->addLegend('clients_legend', 'amg_legend', PaletteManipulator::POSITION_BEFORE)
-	->addField('clients', 'clients_legend', PaletteManipulator::POSITION_APPEND)
-	->applyToPalette('extend', 'tl_user')
-	->applyToPalette('custom', 'tl_user')
+    ->addLegend('clients_legend', 'amg_legend', PaletteManipulator::POSITION_BEFORE)
+    ->addField('clients', 'clients_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('extend', 'tl_user')
+    ->applyToPalette('custom', 'tl_user')
 ;
 
 // Add fields to tl_user
-$GLOBALS['TL_DCA']['tl_user']['fields']['clients'] = array
-(
-	'inputType'               => 'checkbox',
-	'foreignKey'              => 'tl_company_client_group.title',
-	'eval'                    => array('multiple'=>true),
-	'sql'                     => array('type'=>'blob', 'length'=>AbstractMySQLPlatform::LENGTH_LIMIT_BLOB, 'notnull'=>false),
-	'relation'                => array('type'=>'hasMany', 'load'=>'lazy')
-);
+$GLOBALS['TL_DCA']['tl_user']['fields']['clients'] =
+[
+    'inputType' => 'checkbox',
+    'foreignKey' => 'tl_company_client_group.title',
+    'eval' => ['multiple' => true],
+    'sql' => ['type' => 'blob', 'length' => AbstractMySQLPlatform::LENGTH_LIMIT_BLOB, 'notnull' => false],
+    'relation' => ['type' => 'hasMany', 'load' => 'lazy'],
+];
