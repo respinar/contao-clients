f0ce93f4d8672f24768610afc97e5af762e9e872
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-30 07:55:14 +0330

    Add CompanyClientGroupModel for group references

---
+++ src/Model/CompanyClientGroupModel.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Clients.
+ *
+ * (c) Hamid Peywasti
+ *
+ * @license MIT
+ */
+
+namespace Respinar\ClientsBundle\Model;
+
+use Contao\Model;
+
+class CompanyClientGroupModel extends Model
+{
+    protected static $strTable = 'tl_company_client_group';
+}
