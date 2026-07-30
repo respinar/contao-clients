60bd8d1093cfaa87b84a34cfa8f323a0c45b8153
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-30 07:55:37 +0330

    Update label callback to child_record_callback for mode 4 tree view

--- src/EventListener/DataContainer/CompanyClientLabelCallbackListener.php
+++ src/EventListener/DataContainer/CompanyClientLabelCallbackListener.php
@@ -13,21 +13,25 @@ declare(strict_types=1);
 namespace Respinar\ClientsBundle\EventListener\DataContainer;
 
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
-use Contao\DataContainer;
 use Contao\Image;
 use Contao\StringUtil;
 
-#[AsCallback('tl_company_client', 'list.label.label_callback')]
+#[AsCallback('tl_company_client', 'list.sorting.child_record_callback')]
 class CompanyClientLabelCallbackListener
 {
-    public function __invoke(array $row, string $label, DataContainer $dc, array $args): array
+    public function __invoke(array $row): string
     {
+        $label = '<strong>' . $row['name'] . '</strong>';
+
         if ($row['logo']) {
             $uuid = StringUtil::binToUuid($row['logo']);
-            $logo = Image::getHtml($uuid, '', 'style="max-height:32px;max-width:50px;vertical-align:middle;margin-right:8px;"');
-            $args[0] = $logo . ' ' . $args[0];
+            $label = Image::getHtml($uuid, '', 'style="max-height:32px;max-width:50px;vertical-align:middle;margin-right:8px;"') . ' ' . $label;
         }
 
-        return $args;
+        if ($row['industry']) {
+            $label .= ' <span style="color:#999;padding-left:3px">[' . $row['industry'] . ']</span>';
+        }
+
+        return $label;
     }
 }
