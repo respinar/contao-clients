f8fb36370d483f841b19d2c3c32ae01937d813e1
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-30 09:10:50 +0330

    Clean up group DCA: remove unused import and default operations

--- contao/dca/tl_company_client_group.php
+++ contao/dca/tl_company_client_group.php
@@ -8,7 +8,6 @@
  * @license MIT
  */
 
-use Contao\Backend;
 use Contao\DC_Table;
 
 $GLOBALS['TL_DCA']['tl_company_client_group'] = [
@@ -34,37 +33,6 @@ $GLOBALS['TL_DCA']['tl_company_client_group'] = [
         'label' => [
             'fields' => ['title'],
             'format' => '%s',
-        ],
-        'global_operations' => [
-            'all' => [
-                'href'       => 'act=select',
-                'class'      => 'header_edit_all',
-                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
-            ],
-        ],
-        'operations' => [
-            'edit' => [
-                'href'  => 'table=tl_company_client',
-                'icon'  => 'edit.svg',
-            ],
-            'editheader' => [
-                'href'       => 'act=edit',
-                'icon'       => 'header.svg',
-                'attributes' => 'class="edit-header"',
-            ],
-            'copy' => [
-                'href'  => 'act=copy',
-                'icon'  => 'copy.svg',
-            ],
-            'delete' => [
-                'href'       => 'act=delete',
-                'icon'       => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '') . '\'))return false;Backend.getScrollOffset()"',
-            ],
-            'show' => [
-                'href'  => 'act=show',
-                'icon'  => 'show.svg',
-            ],
         ],
     ],
 
