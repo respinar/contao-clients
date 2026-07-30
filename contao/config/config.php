0e350363b1b9800830b442377c903cff042d6683
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-30 07:55:21 +0330

    Register client group table and model in configuration

--- contao/config/config.php
+++ contao/config/config.php
@@ -8,16 +8,18 @@
  * @license MIT
  */
 
+use Respinar\ClientsBundle\Model\CompanyClientGroupModel;
 use Respinar\ClientsBundle\Model\CompanyClientModel;
 
 /*
  * Backend modules
  */
 $GLOBALS['BE_MOD']['company']['company_clients'] = [
-    'tables' => ['tl_company_client']
+    'tables' => ['tl_company_client_group', 'tl_company_client'],
 ];
 
 /*
  * Models
  */
-$GLOBALS['TL_MODELS']['tl_company_client'] = CompanyClientModel::class;
+$GLOBALS['TL_MODELS']['tl_company_client']       = CompanyClientModel::class;
+$GLOBALS['TL_MODELS']['tl_company_client_group'] = CompanyClientGroupModel::class;
