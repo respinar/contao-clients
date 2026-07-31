1fdcd1135585919eaa8407b8eaec2c449a228773
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-31 12:07:48 +0330

    Add English translations for client_list content element

---
+++ contao/languages/en/tl_content.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of Contao Clients.
+ *
+ * (c) Hamid Peywasti
+ *
+ * @license MIT
+ */
+
+$GLOBALS['TL_LANG']['CTE']['client_list'] = ['Client list', 'Display a list of client logos from a selected group.'];
+
+$GLOBALS['TL_LANG']['tl_content']['client_legend'] = 'Client settings';
+$GLOBALS['TL_LANG']['tl_content']['clientGroup'] = ['Client group', 'Please select a client group whose clients should be displayed.'];
